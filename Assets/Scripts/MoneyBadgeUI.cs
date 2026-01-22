7866ccc6df4c9e9dcedbbc7d5a0797aec01c106b
SchaneteaEsguerra <107695838+schan321@users.noreply.github.com>
2026-01-22 14:07:48 +0100

    Added money system and wallet logic, added buttons for testing of money system

---
+++ Assets/Scripts/MoneyBadgeUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class MoneyBadgeUI : MonoBehaviour
+{
+    [SerializeField] private PlayerWallet wallet;
+    [SerializeField] private TMP_Text moneyText;
+
+    private void OnEnable()
+    {
+        if (wallet != null)
+            wallet.OnMoneyChanged += UpdateText;
+    }
+
+    private void OnDisable()
+    {
+        if (wallet != null)
+            wallet.OnMoneyChanged -= UpdateText;
+    }
+
+    private void Start()
+    {
+        if (wallet != null)
+            UpdateText(wallet.Money);
+    }
+
+    private void UpdateText(int amount)
+    {
+        if (moneyText != null)
+            moneyText.text = amount.ToString();
+    }
+}
---
+++ Assets/Scripts/PlayerWallet.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class PlayerWallet : MonoBehaviour
+{
+    [SerializeField] private int startingMoney = 0;
+
+    public int Money { get; private set; }
+
+    public event Action<int> OnMoneyChanged;
+
+    private void Awake()
+    {
+        Money = startingMoney;
+        OnMoneyChanged?.Invoke(Money);
+    }
+
+    public void Add(int amount)
+    {
+        if (amount <= 0) return;
+        Money += amount;
+        OnMoneyChanged?.Invoke(Money);
+    }
+
+    public bool Spend(int amount)
+    {
+        if (amount <= 0) return true;
+        if (Money < amount) return false;
+
+        Money -= amount;
+        OnMoneyChanged?.Invoke(Money);
+        return true;
+    }
+}
+
